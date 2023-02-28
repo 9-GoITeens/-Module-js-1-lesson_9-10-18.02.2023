6c86729473abc91f29fdcf937aba6d002b1329bd
Oksana Petryshyn <opetryshyn2@gmail.com>
2023-02-28 12:02:44 +0200

    hw

--- practice.js
+++ practice.js
@@ -5,6 +5,22 @@
 // Якщо користувач ввів 2 - показуєм слово Зима
 // Якщо 3 -Весна і т.д.
 
+// const monthNumber = Number(prompt('enter month number'));
+
+
+// if (monthNumber === 12 || monthNumber === 1 || monthNumber === 2) {
+//     console.log("Зима")
+// } else if (monthNumber >= 3 && monthNumber <= 5) {
+//     console.log('Весна');
+// } else if (monthNumber >= 6 && monthNumber <= 8) {
+//     console.log('Літо')
+// } else if (monthNumber >= 9 && monthNumber <= 11) {
+//     console.log('Осінь')
+// } else {
+//     console.log('дані не валідні')
+// }
+
+
 // ............
 // Задача №2
 
@@ -12,8 +28,24 @@
 //  і виводить привітання - якщо все добре
 //  і якщо ні помилку - невінний логін
 
+// const enterLogin = prompt('Введіть логін');
+// const enterPassword = prompt('Введіть пароль');
+
 // const login = 'user@gmail.com';
-// const password = '12345'
+// const password = '12345';
+
+// if (login === enterLogin && password === enterPassword) {
+//     console.log('Welcome')
+// } else {
+//     console.log('Error')
+// }
+
+// тернарний оператор
+// const logIn = login === enterLogin && password === enterPassword ? 'Welcome' : 'Error';
+
+// console.log(logIn);
+
+
 
 // ............
 // Задача №3
@@ -31,17 +63,76 @@
 // const max = 5;
 // let total = 0;
 
+// I варіант розвязку
+// for (let i = min; i <= max; i += 1){
+//     if (i % 2 === 0) {
+//         console.log('Парні числа', i);
+
+//         total += i;
+//     }
+// }
+// console.log('Сума парних чисел', total);
+
+// ІІ варіант розвязку
+// Спочатку ми скажемо якщо число непарне то пропусти цю ітерацію
+// for (let i = min; i <= max; i += 1) {
+//     if (i % 2 !== 0) {// !== логічне не
+//         console.log('Непарні числа', i);
+//         continue;//пропускаэ поточну ытерацыю
+       
+//     }
+//     console.log('Парні числа', i)
+//      total += i;
+// }
+// console.log('Сума парних чисел', total);
+
 //..............................
 // Задача №4
 // Порахувати загальну суму в корзині
-// const cart = [54, 28, 105, 70, 92, 17, 120];
-// let total;
+// const carts = [54, 28, 105, 70, 92, 17, 120];
+// const lastElement = carts.length - 1;//створили змінну для останнього елемента
+// let total=0;
 
+// І варіант розвязку
+// for (let i = 0; i <= lastElement; i += 1){
+//     console.log(carts[i]);//отримали доступ до кожного елемента
+//     total += carts[i];//приплюсували кожен елемент
+
+// }
+// console.log('Сума усіх елементів', total);
+
+// ІІ варіант розвязку
+// Також цю задачу можна розвязати за допомогою for of,
+// оскільки нам не потрібно доступ до кожного елемента
+// for (const card of carts) {
+//     total += card;
+// }
+// console.log('Сума усіх елементів', total);
 // ...........................
 
 // Задача №5
 // Напиши скрипт який підраховує суму усіх парних чисел в масиві
+
 // const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
-// let total;
+// let total=0;
 
+// for (let i = 0; i <= numbers.length; i += 1){
+//     if (numbers[i] % 2 === 0) {
+//         console.log('Парні числа', numbers[i])
+        
 
+//         total += numbers[i];
+//     }
+// }
+// console.log('Сума парних чисел', total);
+
+//Також ми можемо цю задачку розвязати на for of
+// for (const number of numbers) {
+//     if (number % 2 === 0) {
+//         console.log('Парні числа', number)
+
+//         total += number;
+//     }
+    
+// }
+// console.log(total);
